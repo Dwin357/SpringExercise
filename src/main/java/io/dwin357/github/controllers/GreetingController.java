6b086831b9dd657a22392ff942571e7aeea011cc
Dwin <e.r.steinmetz@gmail.com>
2017-08-05 13:46:51 -0400

    Initial mvc hello world

---
+++ src/main/java/io/dwin357/github/controllers/GreetingController.java
@@ -0,0 +1,23 @@
+package io.dwin357.github.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+
+	public GreetingController() {
+		super();
+	}
+	
+	@RequestMapping(value="/greet")
+	public String greetPerson (Model model) {
+		
+		model.addAttribute("salutation", "whut up dawg");
+		
+		// this is the string of the view to be used
+		return "hi";
+	}
+
+}
